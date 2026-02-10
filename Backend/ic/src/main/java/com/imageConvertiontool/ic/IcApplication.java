737f5dd8a5d006328b8c5b78860f0313ee3124e3
Poovendran <pooveshanmugam@gmail.com>
2026-02-10 14:11:07 +0530

    This is base files for the project

---
+++ Backend/ic/src/main/java/com/imageConvertiontool/ic/IcApplication.java
@@ -0,0 +1,13 @@
+package com.imageConvertiontool.ic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcApplication.class, args);
+	}
+
+}
